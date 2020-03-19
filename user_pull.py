d5849ab8684d70fe7f96377010d820cbf9e42224
Chernide <denver.chernin@colorado.edu>
2020-03-18 20:10:54 -0600

    Added trip_id to trip dashboard

--- user_pull.py
+++ user_pull.py
@@ -22,16 +22,17 @@ def get_upcoming_trips(uid):
     q_email = "select email from users where user_id = {}".format(uid)
     cursor.execute(q_email)
     email = cursor.fetchone()[0]
-    q_trips = "select tripname, loc_id, TO_CHAR(trip_start,  'MM/DD/YYYY'), TO_CHAR(trip_end,  'MM/DD/YYYY') from trips where user_id = {} or members like '%{}%'".format(uid, email)
+    q_trips = "select trip_id, tripname, loc_id, TO_CHAR(trip_start,  'MM/DD/YYYY'), TO_CHAR(trip_end,  'MM/DD/YYYY') from trips where user_id = {} or members like '%{}%'".format(uid, email)
     cursor.execute(q_trips)
     trips = cursor.fetchall()
     for t in trips:
         d = {}
-        d['name'] = t[0]
-        q_loc = "select city from destinations where dest_id = {}".format(t[1])
+        d['trip_id'] = t[0]
+        d['name'] = t[1]
+        q_loc = "select city from destinations where dest_id = {}".format(t[2])
         cursor.execute(q_loc)
         d['location'] = cursor.fetchone()[0]
-        d['start'] = t[2]
-        d['end'] = t[3]
+        d['start'] = t[3]
+        d['end'] = t[4]
         ret["Trips"].append(d)
     return ret
