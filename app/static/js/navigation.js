b9c4194e493ec957071b198743a36f5b8eff144f
Serena Buxton <serena.buxton@colorado.edu>
2020-04-08 15:40:05 -0600

    updated nav

---
+++ app/static/js/navigation.js
@@ -0,0 +1,39 @@
+function drawerToggle(){
+    var element = document.getElementsByClassName("side_drawer")[0];
+    var classes = element.classList;
+    backdrop = document.getElementById("backdrop_container");
+    var open = false;
+    for (var i=0; i < classes.length; i++){
+        if (classes[i] == "open"){
+            open = true;
+            break;
+        }
+    }
+    if(open){
+        element.classList.remove("open");
+        backdrop.classList.remove("backdrop");
+        backdrop.onclick = "";
+    }
+    else{
+        element.classList.add("open");
+        backdrop.classList.add("backdrop");
+        backdrop.onclick = drawerToggle;
+    }
+}
+
+
+function toggle_settings_dropdown() {
+    var elements = document.getElementsByClassName("dropdown-content");
+    for(var i=0; i < elements.length; i++){
+        elements[i].classList.toggle("show");
+        elements[i].classList.toggle("hidden");
+    }
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      document.getElementById("settings_dropdown").classList.remove("show");
+    document.getElementById("settings_dropdown").classList.add("hidden");
+    }
+  }
